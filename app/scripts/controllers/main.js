7431c686471218f550a6c10096f2047e3c61a513
bguillon-td <bguillon@talend.com>
2015-12-09 21:53:49 +0100

    initial commit - app generated by yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name todo1App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the todo1App
+ */
+angular.module('todo1App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
